27da2414bd878ef6aab7a1aed13d78caf1903676
Ruslan Zapolianenko <rusartrade@gmail.com>
2023-11-24 00:33:11 +0200

    commit editnote17-2

--- src/main/java/org/example/service/NoteService.java
+++ src/main/java/org/example/service/NoteService.java
@@ -1,45 +1,37 @@
 package org.example.service;
 
 import org.example.entity.Note;
+import org.example.repository.NoteRepository;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class NoteService {
-    private final List<Note> notes = new ArrayList<>();
-    private final AtomicLong idGenerator = new AtomicLong(1);
+
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
 
     public List<Note> listAll() {
-        return notes;
+        return noteRepository.findAll();
     }
 
     public Note add(Note note) {
-        note.setId(idGenerator.getAndIncrement());
-        notes.add(note);
-        return note;
+        return noteRepository.save(note);
     }
 
     public void deleteById(long id) {
-        notes.removeIf(note -> note.getId() == id);
+        noteRepository.deleteById(id);
     }
 
     public void update(Note note) {
-        for (int i = 0; i < notes.size(); i++) {
-            if (notes.get(i).getId().equals(note.getId())) {
-                notes.set(i, note);
-                return;
-            }
-        }
+        noteRepository.save(note);
     }
 
     public Note getById(long id) {
-        for (Note note : notes) {
-            if (note.getId() == id) {
-                return note;
-            }
-        }
-        throw new RuntimeException("Note not found with id: " + id);
+        return noteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Note not found with id: " + id));
     }
 }
