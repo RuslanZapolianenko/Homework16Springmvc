8d96e203a4ac98eb2fda28eda4129e662d601f93
Ruslan Zapolianenko <rusartrade@gmail.com>
2023-11-28 11:58:56 +0200

    commit editnote18

--- src/main/java/org/example/entity/Note.java
+++ src/main/java/org/example/entity/Note.java
@@ -14,5 +14,11 @@ public class Note {
     private Long id;
     private String title;
     private String content;
+
+    public Note(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
 }
 
